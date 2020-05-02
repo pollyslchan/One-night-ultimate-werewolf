df60ecbe4519a50eadacfc584887fff950d36482
Polly <polly.chan@live.com>
2020-05-02 03:34:41 +0000

    add start script

--- app.js
+++ app.js
@@ -242,6 +242,6 @@ app.get("/results", function(req, res){
 	});
 });
 
-app.listen(3000, function(){
-	console.log("Server listening on port 3000")
+app.listen(process.env.PORT || 3000, function(){
+	console.log("Server listening")
 });
